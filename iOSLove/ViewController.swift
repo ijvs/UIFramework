6df003000bd8df937acc3c1ba38fdd3dc6cc3a2a
Israel Jonathan Velázquez Sánchez <ijonathanvs@gmail.com>
2018-10-26 17:51:09 -0300

    Initial Commit

---
+++ iOSLove/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSLove
+//
+//  Created by Israel Jonathan Velázquez Sánchez on 10/26/18.
+//  Copyright © 2018 AVACO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
